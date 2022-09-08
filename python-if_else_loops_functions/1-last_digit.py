f493a2c4a078b0abac510e0fdadd5ca7fe280b51
Drixner <drixnerrafael1@gmail.com>
2022-09-08 00:24:23 -0500

    Run a program that print value random

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+print(number)
+if number < 0:
+    last_digit = number % -10
+elif number:
+    last_digit = number % 10
+
+if last_digit > 5:
+    print("Last digit of", number, "is", last_digit,"and is greater than 5")
+if last_digit == 5:
+    print("Last digit of", number, "is", last_digit,"and is 0")
+if last_digit < 6 and not 0:
+    print("Last digit of", number, "is", last_digit,"nd is less than 6 and not 0")
