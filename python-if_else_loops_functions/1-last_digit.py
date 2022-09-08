bf5c4c69f4c9abec46636a679667cb275b7afd9e
Drixner <drixnerrafael1@gmail.com>
2022-09-08 00:27:26 -0500

    This program will assign a random signed number

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -8,8 +8,8 @@ elif number:
     last_digit = number % 10
 
 if last_digit > 5:
-    print("Last digit of", number, "is", last_digit,"and is greater than 5")
+    print("Last digit of", number, "is", last_digit," and is greater than 5")
 if last_digit == 5:
-    print("Last digit of", number, "is", last_digit,"and is 0")
+    print("Last digit of", number, "is", last_digit," and is 0")
 if last_digit < 6 and not 0:
-    print("Last digit of", number, "is", last_digit,"nd is less than 6 and not 0")
+    print("Last digit of", number, "is", last_digit," and is less than 6 and not 0")
