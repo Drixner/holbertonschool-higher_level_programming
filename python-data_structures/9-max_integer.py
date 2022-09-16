852736b33703bc0c6a0956d52132c27ec2fbbfa5
Drixner <drixnerrafael1@gmail.com>
2022-09-15 23:58:05 -0500

    Write a function that finds the biggest integer of a list.

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list == False:
+    if my_list == 0:
         return None
-    else:
-        new = my_list.sort()
-        return my_list[-1]
+    new = my_list.sort()
+    return my_list[-1]
