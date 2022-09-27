8621428eab5d740b110989be2f65bb613dc679e7
Drixner <drixnerrafael1@gmail.com>
2022-09-27 12:21:11 -0500

    Write a class LockedClass with no class or object attribute

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,14 +1,4 @@
 #!/usr/bin/python3
-"""
-Locked Class with no class or object attributte
-That prevents the user from dinamically creating
-new instance attribute is called first_name
-"""
-
-
 Class lockedClass():
-    """
-    Locked class
-    """
-
+"""bloqueando la clases para que no registre atributos dinamicamente usando slots"""
     __slots__ = ["first_name"]
