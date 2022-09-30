bb5185585ace8692d36e056b7245be1adb70915e
Drixner <drixnerrafael1@gmail.com>
2022-09-30 16:50:29 -0500

    task6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Definition of the class
+"""
+
+
+class BaseGeometry():
+    """Attributes by class public"""
+    def area(self):
+        """raise call exception"""
+        raise Exception("area() is not implemented")
