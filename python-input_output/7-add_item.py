322e1b1682ed6091435ed852be24b9a31cbcfd88
Drixner <drixnerrafael1@gmail.com>
2022-10-03 15:54:48 -0500

    task7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,6 +4,7 @@ Script that adds all arguments to a Python list, and then saves them to a file
 """
 
 from sys import argv
+"""the description"""
 save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
 
