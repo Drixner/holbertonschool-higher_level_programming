582c3ed76f27da298b065d30f5a83d87ec19dc61
Drixner <drixnerrafael1@gmail.com>
2022-10-06 14:25:51 -0500

    first commit models

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""this module have the base class
+"""
+
+
+class Base:
+    """a class base"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """init the class base"""
+
