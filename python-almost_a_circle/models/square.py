2a42526b1381d27b67fbddf25a4b83a60b1103a0
Drixner <drixnerrafael1@gmail.com>
2022-10-07 16:40:13 -0500

    pdate the class Square by adding the public getter and setter size

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+this module is the class squere
+"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """A representation of a square"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """initializes the square"""
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    @property
+    def size(self):
+        """getter"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """setter"""
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """informal string representation of the square"""
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id, self.x,
