2201c6e99d57786d813bad5b5fd09980fc12ad2a
Drixner <drixnerrafael1@gmail.com>
2022-11-05 15:36:05 -0500

    Writing a script what delete database not failclear

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 -- this script deletes a database, and does not fail if the database does not exist
 -- DROP DATABASE IF EXISTS hbtn_0c_0;
-CREATE DATABASE hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
