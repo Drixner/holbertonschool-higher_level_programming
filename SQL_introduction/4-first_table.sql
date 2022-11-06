f8fdefdbccecd22baa65ab2efba0b1ac3d471877
Drixner <drixnerrafael1@gmail.com>
2022-11-05 19:41:08 -0500

    creating first table

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- This script creates a table and defines the properties.
-CREATE TABLE IF NOT EXISTS firs_table (id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS `firs_table` (`id` INT, `name` VARCHAR(256));
