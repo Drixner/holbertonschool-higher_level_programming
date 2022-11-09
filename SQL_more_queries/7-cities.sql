bb430e06e795976c5f5e33fb3d4dfa469aee466c
Drixner <drixnerrafael1@gmail.com>
2022-11-09 18:08:36 -0500

    Write a script that creates the database hbtn_0d_usa

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -5,4 +5,4 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
 state_id INT NOT FULL,
 name VARCHAR(256) NOT FULL,
 PRIMARY KEY (id),
-FOREIGN KEY (state_id) REFERENCE hbtn_0d_usa.states(id));
+FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id));
