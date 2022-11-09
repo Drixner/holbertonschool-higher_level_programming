2a71bb508623fe78c49c924c64ecdb8a14ca2778
Drixner <drixnerrafael1@gmail.com>
2022-11-09 18:04:26 -0500

    Write a script that creates the database hbtn_0d_usa

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- This script creates a database and tables with certain characteristics.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLA IF NOT EXISTS hbtn_0d_usa.cities
+(id INT NOT FULL UNIQUE AUTO_INCREMENT,
+state_id INT NOT FULL,
+name VARCHAR(256) NOT FULL,
+PRIMARY KEY(id),
+FOREING KEY(state_id) REFERENCE hbtn_0d_usa.states(id))
