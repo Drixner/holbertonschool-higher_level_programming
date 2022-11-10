e376dcb383823078d25760a6327a07bf721bcd07
Drixner <drixnerrafael1@gmail.com>
2022-11-10 07:47:15 -0500

    this script that lists all comedy show in db

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,9 @@
--- script that lists all shows and all genre linked to that show
-SELECT tv_shows.title, tv_genres.name FROM tv_shows
-LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT OUTER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title, tv_genres.name;
+-- script that lists all comedy show in db
+SELECT tv_shows.title 
+  FROM tv_shows 
+  INNER JOIN tv_show_genres 
+  ON tv_shows.id = tv_show_genres.show_id 
+  INNER JOIN tv_genres 
+  ON tv_show_genres.genre_id = tv_genres.id 
+  WHERE tv_genres.name = 'Comedy' 
+  ORDER BY tv_shows.title;
