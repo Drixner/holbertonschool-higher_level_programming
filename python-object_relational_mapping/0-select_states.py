8e4ac11cda0b0755c5713a3566ecf5e938898149
Drixner <drixnerrafael1@gmail.com>
2022-11-11 22:03:20 -0500

    Write a script that lists all states from the database hbtn_0e_0_usa

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,10 +4,10 @@ This script lists all the states of the database hbtn_0e_0_usa.
 """
 
 
+if __name__ == '__main__':
 import MySQLdb
 from sys import argv
 
-if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
             passwd=argv[2], db=argv[3])
 
