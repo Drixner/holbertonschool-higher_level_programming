4302020569ccbfd05ecfe2f29f0852da71ad8578
Drixner <drixnerrafael1@gmail.com>
2022-12-10 23:51:07 -0500

    fix: error typo in argument

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -4,4 +4,4 @@
 const process = require('process'); // agrego el modulo process
 const args = process.argv; // defino args como process.argv
 
-console.log(args[2] = 'is' + args[3]);
+console.log(args[2] + ' is ' + args[3]);
