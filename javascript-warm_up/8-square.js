9bc9268f428d9c166190b19a0ae829711fefa79b
Drixner <drixnerrafael1@gmail.com>
2022-12-11 10:46:55 -0500

    feat: script that print a square

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/nope
+// Script that prints a square
+// More info > https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
+
+const process = require('process'); // se agrega el modulo process usando "require"
+const args = process.argv; // se define argv para capturar el valor, para luego ser manipulado
+const size = args[2];
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+}
