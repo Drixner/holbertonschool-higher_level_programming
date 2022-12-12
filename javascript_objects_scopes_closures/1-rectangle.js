81246716f3b0c7eb14777e308de76615c67ba632
Drixner <drixnerrafael1@gmail.com>
2022-12-12 06:08:00 -0500

    fix: correct error typo

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -2,7 +2,7 @@
 // Aca se usa el constructor para definir las variables "w" y "h". Y despues se inicializan
 // las variables que se cran "width" y "height" para cada objeto usando "this" con los valores
 // que se pasen como argumento en "w" y "h".
-module.export = class Rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
     this.width = w;
     this.height = h;
