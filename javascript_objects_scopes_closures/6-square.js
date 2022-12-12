f471682465a056f7599579c2eac19a11e0ffdd11
Drixner <drixnerrafael1@gmail.com>
2022-12-12 06:21:19 -0500

    feat: write a class Square that defines a square and inherits from Square of 5-square.js

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+// Write a class Square that defines a square and inherits from Rectangle of 4-rectangle.js
+// Importamos la clase Rectangle
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size); // la clase Rectangle recibe 2 parametros, por eso le paso dos veces "size"
+  }
+
+  // add method "charPrint(c)"
+  charPrint (c = 'X') { // seteo 'X' como valor default si no se pasa nada como argumento.
+    for (let i = 0; i < this.height; i++) { // se puede usar tanto "height" como "width" de la class Rectangle para imprimir el Rectangle.
+      console.log(c.repeat(this.height));
+    }
+  }
+}
+
+// Se exporta el modulo para que se pueda usar por los demas scripts (el script 6-main.js)
+module.exports = Square;
