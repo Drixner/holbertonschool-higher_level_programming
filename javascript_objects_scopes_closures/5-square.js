2e65165db2661b8480754e14dfdda271d490747d
Drixner <drixnerrafael1@gmail.com>
2022-12-12 06:16:54 -0500

    feat: write a class Square that defines a square and inherits from Rectangle of `4-rectangle.js`

---
+++ javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// Write a class Square that defines a square and inherits from Rectangle of 4-rectangle.js:
+// Use of extends: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/extends
+// Use of super: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/super
+
+// Importamos la clase Rectangle
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size); // la clase Rectangle recibe 2 parametros, por eso le paso dos veces "size"
+  }
+}
+
+// Se exporta el modulo para que se pueda usar por los demas scripts (el script 6-main.js)
+// Esta es una variante a la forma en la que venia poniendo el export en los ejs anteriores.
+module.exports = Square;
