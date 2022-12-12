55f04bcca27f7d1d8ec87f01a7646cae9839e48c
Drixner <drixnerrafael1@gmail.com>
2022-12-12 06:05:44 -0500

    feat: script  a class Rectangle that defines a rectangle

---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Aca se usa el constructor para definir las variables "w" y "h". Y despues se inicializan
+// las variables que se cran "width" y "height" para cada objeto usando "this" con los valores
+// que se pasen como argumento en "w" y "h".
+module.export = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
