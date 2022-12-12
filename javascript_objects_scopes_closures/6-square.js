2423dfcf3cea924272b17b9d499334fbf69902ab
Drixner <drixnerrafael1@gmail.com>
2022-12-12 06:46:54 -0500

    feat: Write a class Square that defines a square and inherits from Rectangle of 6-square.js

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-// Write a class Square that defines a square and inherits from Rectangle of 4-rectangle.js
+// Write a class Square that defines a square and inherits from Rectangle of 5-square.js
 /*
 - You must use the class notation for defining your class and extends
 - Create an instance method called charPrint(c) that prints the rectangle using the character c
 If c is undefined, use the character X
 */
 // Importamos la clase Rectangle
-const Rectangle = require('./4-rectangle');
+const Rectangle = require('./5-square.js');
 
 class Square extends Rectangle {
   constructor (size) {
