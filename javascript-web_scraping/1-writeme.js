8342879bf904e2b9d28a30affff3c960cd1a99a3
Drixner <drixnerrafael1@gmail.com>
2022-12-12 23:54:54 -0500

    feat: write a script that writes a string to a file

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+/*
+Write a script that writes a string to a file.
+
+    The first argument is the file path
+    The second argument is the string to write
+    The content of the file must be written in utf-8
+    If an error occurred during while writing, print the error object
+*/
+
+const process = require('process'); //
+const args = process.argv;
+
+const filePath = args[2];
+const content = args[3];
+
+const fs = require('fs');
+
+fs.writeFile(filePath, content, err => {
+  if (err) {
+    console.error(err);
+  }
+});
