b521a70c3aa2b512aa35dec7abe5dd740c89f1b5
Drixner <drixnerrafael1@gmail.com>
2022-12-14 16:11:10 -0500

    feat: write a script that gets the contents of a webpage and stores it in a file.

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+const process = require('process');
+const request = require('request');
+
+// El primer argumento es la URL a solicitar
+const url = process.argv[2];
+
+// El segundo argumento es la ruta del archivo donde se almacenará la respuesta del cuerpo
+const filePath = process.argv[3];
+
+// Realizar una solicitud GET a la URL especificada
+request.get(url, (error, response, body) => {
+  if (error) {
+    // Manejar el error si ocurre uno
+    console.error(error);
+  } else {
+    // Escribir la respuesta del cuerpo en la ruta del archivo especificada,
+    // usando la codificación de caracteres UTF-8
+    require('fs').writeFile(filePath, body, 'utf8', err => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(`Se guardó la respuesta del cuerpo en ${filePath}`);
+      }
+    });
+  }
+});
+
