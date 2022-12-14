6ac149796009306fba9df54512ad3ef4ce8e17fa
Drixner <drixnerrafael1@gmail.com>
2022-12-13 23:34:48 -0500

    fix: correct `expected error to be handled`

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// add module request
+// import module request
 const request = require('request');
 
 // Get URL from command line argument
@@ -7,6 +7,18 @@ const url = process.argv[2];
 
 // Send GET request to URL
 request(url, (error, response, body) => {
-  // Print status code
-  console.log(`code: ${response.statusCode}`);
+  // Check if there was an error
+  if (error) {
+    // Print error message
+    console.error('There was an error while making the request:', error);
+  } else {
+    // Check if response is not null
+    if (response) {
+      // Print status code
+      console.log(`code: ${response.statusCode}`);
+    } else {
+      // Print error message
+      console.error('No response received from the server');
+    }
+  }
 });
