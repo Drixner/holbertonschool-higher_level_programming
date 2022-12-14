baa9b0fb658a864b7cc3c3b66a1f221958bc7f4a
Drixner <drixnerrafael1@gmail.com>
2022-12-14 14:56:15 -0500

    feat: write a script that prints the number of movies where the character “Wedge Antilles” is present.

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,24 +1,24 @@
 #!/usr/bin/node
 
-const request = require('request');
+const request = require("request");
 const API_URL = process.argv[2];
 const CHARACTER_ID = 18;
 
-request(API_URL, function (error, response, body) {
+request(API_URL, function(error, response, body) {
   if (!error && response.statusCode === 200) {
-    const movies = JSON.parse(body).results;
+    let movies = JSON.parse(body).results;
     let count = 0;
     for (let i = 0; i < movies.length; i++) {
-      const movie = movies[i];
+      let movie = movies[i];
       for (let j = 0; j < movie.characters.length; j++) {
-        const character = movie.characters[j];
+        let character = movie.characters[j];
         if (character.includes(CHARACTER_ID)) {
           count++;
         }
       }
     }
-    console.log(`${count}`);
+    console.log(`Wedge Antilles is present in ${count} movies.`);
   } else {
-    console.log('An error occurred while trying to access the API.');
+    console.log("An error occurred while trying to access the API.");
   }
 });
