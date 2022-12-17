1681c76f2c889d735101e9a81ba7611d1fa5dca0
Drixner <drixnerrafael1@gmail.com>
2022-12-17 18:23:05 -0500

    feat: use api query to modify color

---
+++ javascript-web_jquery/1-script.js
@@ -0,0 +1,11 @@
+/*
+* Write a JavaScript script that updates the text color of the <header> element to red (#FF0000):
+
+    You can’t use document.querySelector to select the HTML tag
+    You must use the JQuery API
+*/
+const $ = window.$;
+
+$(function() {
+  $('header').css('color', '#FF0000');
+})
